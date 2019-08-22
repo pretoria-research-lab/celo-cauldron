aaa45acbce22347bc38daf21c30979ea8425f7eb
Aaron Boyd <aaronmboyd@gmail.com>
2019-08-22 15:04:43 +0200

    Updating CI/CD pipeline for S3

---
+++ app/build/precache-manifest.322993e8e08e1a7b6f8cfcf2e3c38dd1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebdefc354e1ed80076be2c33ad044095",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0b5dc7020e1a3488da7",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "6720f9c24e9f1a5388b9",
+    "url": "/static/css/main.77fb9851.chunk.css"
+  },
+  {
+    "revision": "a0b5dc7020e1a3488da7",
+    "url": "/static/js/2.ff8bc1f5.chunk.js"
+  },
+  {
+    "revision": "6720f9c24e9f1a5388b9",
+    "url": "/static/js/main.ee84230c.chunk.js"
+  },
+  {
+    "revision": "2c8f66eef5c48a521c18",
+    "url": "/static/js/runtime~main.72e64c91.js"
+  },
+  {
+    "revision": "1d66decc28fe3126eaaca104a2430830",
+    "url": "/static/media/loading.1d66decc.svg"
+  }
+]);
--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+cd app
+yarn build 
+cd ..
 serverless syncToS3
 serverless invalidateCloudFrontCache
