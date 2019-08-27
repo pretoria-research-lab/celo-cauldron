740cec806aa1bdad813a7cf06041c882764e1647
Aaron Boyd <aaronmboyd@gmail.com>
2019-08-27 17:30:59 +0200

    Split API configuration dev/prod

---
+++ config_dev.js
@@ -0,0 +1,6 @@
+export const API_CONFIG = {
+    baseUrl : "https://xsxgj1rqnc.execute-api.eu-central-1.amazonaws.com",
+    basePath : "/dev/reward",
+    timeout: 12000,
+    headers: {"Content-Type":"application/json;charset=utf-8"}
+}
