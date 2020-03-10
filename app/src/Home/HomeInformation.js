cd08d27bb8184b29f7962fc09a6ca0c85154715e
Aaron Boyd <aaronmboyd@gmail.com>
2020-03-10 14:44:59 +0100

    Minor layout tweaks

---
+++ app/src/Home/HomeInformation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function HomeInformation(props) {
+
+    return (
+        <div id="home-info" className="row">
+          <div className="col-lg-12">
+              <p>A repository of useful tools, scripts, and information for developing on the Celo ecosystem.</p>
+          </div>
+        </div>
+    );
+}
--- app/src/Home/index.js
+++ app/src/Home/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Jumbotron} from 'react-bootstrap';
 import './home.css';
+import HomeInformation from './HomeInformation';
 import developerLogo from '../assets/01-Developers (light bg).png'
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
           <p className="lead"><img id="developer-logo" src={developerLogo} alt="Developer logo" />Community built tools for developers</p>
           < hr/>          
         </Jumbotron>
+        <HomeInformation />
       </div>
     </div>
   </div>
