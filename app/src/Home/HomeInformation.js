e7ed7efb1b4c46e6a7ae6413567fa08793ff1317
Aaron Boyd <aaronmboyd@gmail.com>
2020-03-10 14:55:21 +0100

    Fix - adding in HomeInformation

--- app/src/Home/HomeInformation.js
+++ app/src/Home/HomeInformation.js
@@ -6,6 +6,7 @@ export default function HomeInformation(props) {
         <div id="home-info" className="row">
           <div className="col-lg-12">
               <p>A repository of useful tools, scripts, and information for developing on the Celo ecosystem.</p>
+              <p>Contributions are welcome at <a href="https://gitlab.com/aaron.boyd/celo-cauldron">https://gitlab.com/aaron.boyd/celo-cauldron</a></p>
           </div>
         </div>
     );
