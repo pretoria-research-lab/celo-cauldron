39c02de37a62b8c1f5eace69355a1679aaa48df1
Aaron Boyd <aaronmboyd@gmail.com>
2020-03-11 15:14:10 +0100

    Missed files

---
+++ app/src/Utils/config.js
@@ -0,0 +1,28 @@
+export const API_CONFIG = [
+    {   network : "alfajores",
+        host: "https://zoli2ggacd.execute-api.eu-central-1.amazonaws.com",
+        basePath: "/alfajores",
+        blockExplorer: "https://alfajores-blockscout.celo-testnet.org/",
+        faucetAddress: "0xC02b8b165CC5a2A731E9C2BD6fdF66eaBcfa8663",
+        remoteNode: "https://alfajores-forno.celo-testnet.org",
+        blocksCooldown: 12
+    },
+    {   network : "baklava",
+        host: "https://zoli2ggacd.execute-api.eu-central-1.amazonaws.com",
+        basePath: "/baklava",
+        blockExplorer: "https://baklava-blockscout.celo-testnet.org/",
+        faucetAddress: "0xC02b8b165CC5a2A731E9C2BD6fdF66eaBcfa8663",
+        remoteNode: "https://baklava-forno.celo-testnet.org",
+        blocksCooldown: 12
+    },
+    {   network : "rc1",
+        host: "https://zoli2ggacd.execute-api.eu-central-1.amazonaws.com",
+        basePath: "/rc1",
+        blocksCooldown: 12
+    },
+    {   network : "mainnet",
+        host: "https://zoli2ggacd.execute-api.eu-central-1.amazonaws.com",
+        basePath: "/mainnet",
+        blocksCooldown: 12
+    }
+]
