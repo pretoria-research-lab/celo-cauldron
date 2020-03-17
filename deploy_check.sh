5e21770ff3a391769a5150cb8386c744e4f616bd
Aaron Boyd <aaronmboyd@gmail.com>
2020-03-17 12:12:05 +0100

    Tweak deploy check script

--- deploy_check.sh
+++ deploy_check.sh
@@ -2,10 +2,20 @@
 
 # First argument is stage identifer
 
-if aws s3api head-bucket --bucket "pretoria-celo-cauldron-$1" 2>/dev/null;
+BUCKET=pretoria-celo-cauldron-$1
+STATUS=$(aws s3api head-bucket --bucket $BUCKET 2>&1)
+
+if echo "$STATUS" | grep 'Not Found';
 then
-    echo "Bucket $1 already exists, do not need to recreate ..."
+  echo "Bucket $BUCKET doesn't exist, deploying first time ...";
+  sls deploy -v --stage $1;
+elif echo "$STATUS" | grep 'Forbidden';
+then
+  echo "Bucket $BUCKET exists but not owned";
+  return 1;
+elif echo "$STATUS" | grep 'Bad Request';
+then
+  echo "Bucket $BUCKET is less than 3 or greater than 63 characters"
 else
-    echo "Bucket $1 does not exist, creating serverless stack from scratch ..."
-    sls deploy -v --stage $1
+  echo "Bucket $BUCKET exists, no further action";
 fi
