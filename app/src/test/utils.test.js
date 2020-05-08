86aae988508b9bf3a27d139ab03051546d122280
Aaron Boyd <aaronmboyd@gmail.com>
2020-05-08 12:54:24 +0200

    [TEST] - Increase timeout for CI/CD

--- app/src/test/utils.test.js
+++ app/src/test/utils.test.js
@@ -1,6 +1,6 @@
 const assert = require("assert");
 const utils = require("../Utils/utils");
-const fornoTimeout = 20000; //ms
+const fornoTimeout = 30000; //ms
 const config = require('../Utils/config');
 
 const baklavaForno = config.API_CONFIG.filter((element) => element.network === 'baklava')[0].remoteNode;
